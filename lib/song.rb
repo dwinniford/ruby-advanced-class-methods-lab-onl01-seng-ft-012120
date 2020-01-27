d670e794a94426069293c4ebde9713f1830dc07f
Dave Winniford <dwinniford@gmail.com>
2020-01-27 20:21:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,10 @@ class Song
   def self.find_by_name(name)
     self.all.find { |song| song.name == name }
   end 
+  
+  def self.find_or_create_by_name(name)
+    if self.all.include?()
+  end 
 
   def save
     self.class.all << self
